ca49cd678f1c428e73f35712cc4c7667a9c1eafe
Phakhawat Chullamonthon <phakhawat.chu@outlook.com>
2022-06-25 22:25:09 +0700

    feat: add post exercise service

--- index.js
+++ index.js
@@ -50,6 +50,36 @@ app.post("/api/users", (req, res) => {
     });
 });
 
+app.post("/api/users/:id/exercises", (req, res) => {
+    const _id = req.params.id;
+    const { description, duration } = req.body;
+    const date = req.body.date ? new Date(req.body.date).toDateString() : new Date().toDateString();
+    User.findById(_id, (err, data) => {
+        if (err) return console.error(err);
+        else {
+            const username = data.username;
+            const exercise = new Exercise({
+                username: username,
+                date: date,
+                description: description,
+                duration: duration,
+            });
+            exercise.save((err, data) => {
+                if (err) return console.error(err);
+                else {
+                    res.json({
+                        _id: data._id,
+                        username: data.username,
+                        date: data.date,
+                        description: data.description,
+                        duration: data.duration,
+                    });
+                }
+            });
+        }
+    });
+});
+
 const listener = app.listen(process.env.PORT || 3000, () => {
     console.log("Your app is listening on port " + listener.address().port);
 });
